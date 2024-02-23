4fb8fa4a2a1fd1eb21e3c6f1291ab363993b01c3
riza569 <rizamuhammed569@gmail.com>
2024-02-23 19:59:02 +0530

    messagehalf

--- server/index.js
+++ server/index.js
@@ -1,9 +1,35 @@
+// const express = require("express");
+// const app = express();
+// const http = require("http");
+// const { Server } = require("socket.io");
+// const cors = require("cors");
+// app.use(cors);
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
+// io.on("connection", (socket) => {
+//   console.log(`user connected${socket.id}`);
+//   socket.on("send_message", (data) => {
+//     socket.broadcast.emit("receive_message", data);
+//   });
+// });
+
+// server.listen(3001, () => {
+//   console.log("server is running");
+// });
+
 const express = require("express");
-const app = express();
 const http = require("http");
+const app = express();
 const { Server } = require("socket.io");
 const cors = require("cors");
+const { Socket } = require("dgram");
 app.use(cors);
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
@@ -11,13 +37,14 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
+
 io.on("connection", (socket) => {
-  console.log(`user connected${socket.id}`);
+  console.log(`user connected:${socket.id}`);
   socket.on("send_message", (data) => {
     socket.broadcast.emit("receive_message", data);
   });
 });
 
 server.listen(3001, () => {
-  console.log("server is running");
+  console.log("first");
 });
